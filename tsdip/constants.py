284c52d6caa02859ad4b1c6b459fbb216228289d
Edwin Jarvis <fenglan.crazydance@gmail.com>
2019-11-13 00:29:59 +0800

    Add patch social to sudio api
    Signed-off-by: Edwin Jarvis <fenglan.crazydance@gmail.com>

--- tsdip/constants.py
+++ tsdip/constants.py
@@ -7,21 +7,27 @@ class ErrorCode(IntEnum):
     ERROR_STUDIO_1 = 1
     ERROR_STUDIO_2 = 2
     ERROR_STUDIO_3 = 3
+    ERROR_STUDIO_4 = 4
+    ERROR_STUDIO_5 = 5
+    ERROR_STUDIO_6 = 6
 
 
 class ErrorMessage(Enum):
     """ """
     ROUTE_AUTH_0 = 'Unexpected error'
-    ERROR_STUDIO_1 = 'API parameters are not valid'
+    ERROR_STUDIO_1 = 'Create studio API parameters are not valid'
     ERROR_STUDIO_2 = 'Create studio fail'
     ERROR_STUDIO_3 = 'Get studios fail'
+    ERROR_STUDIO_4 = 'Patch social to studio API parameters are not valid'
+    ERROR_STUDIO_5 = 'Studio is not exist'
+    ERROR_STUDIO_6 = 'Patch social to studio fail'
 
 
 class SuccessMessage(Enum):
     """ """
     ROUTE_AUTH_1 = 'Create studio success'
     ROUTE_AUTH_2 = 'Get studios success'
-
+    ROUTE_AUTH_3 = 'Patch social to studio success'
 
 class ResponseStatus(Enum):
     """ """
--- tsdip/routes/studio.py
+++ tsdip/routes/studio.py
@@ -6,7 +6,7 @@ from flask import g, request
 from marshmallow import Schema, ValidationError, fields
 
 from tsdip.formatter import format_response
-from tsdip.models import Studio
+from tsdip.models import Social, Studio
 
 api_blueprint = Blueprint('studios', __name__, url_prefix='/studios')
 
@@ -42,10 +42,12 @@ def create():
         )
         g.db_session.add(row)
         g.db_session.commit()
-        g.db_session.flush(row)
-        res = row.to_dict()
+        g.db_session.refresh(row)
+        res = row.as_dict()
     except Exception as err:
         app.logger.error(err)
+        g.db_session.rollback()
+
         return {
             'code': 'ERROR_STUDIO_2',
             'description': str(err),
@@ -93,5 +95,76 @@ def get_list():
             'code': 'ROUTE_AUTH_2',
             'data': result,
             'http_status_code': HTTPStatus.OK,
+            'status': 'SUCCESS',
+        }
+
+
+class SocialSchema(Schema):
+    email = fields.Email()
+    fan_page = fields.Str()
+    instagram = fields.Str()
+    line = fields.Str()
+    telephone = fields.Str()
+    website = fields.URL()
+    youtube = fields.Str()
+
+
+@api_blueprint.route('/<path:studio_id>', methods=['PATCH'])
+@format_response
+def patch_social(studio_id):
+    try:
+        SocialSchema().load(request.get_json())
+    except ValidationError as err:
+        app.logger.error(err.messages)
+        app.logger.error(err.valid_data)
+        return {
+            'code': 'ERROR_STUDIO_4',
+            'description': err.messages,
+            'http_status_code': HTTPStatus.BAD_REQUEST,
+            'status': 'ERROR',
+        }
+
+    data = request.get_json()
+
+    try:
+        studio = g.db_session.query(Studio).get(studio_id)
+
+        if not studio:
+            return {
+                'code': 'ERROR_STUDIO_5',
+                'http_status_code': HTTPStatus.BAD_REQUEST,
+                'status': 'ERROR',
+            }
+
+        row = Social(
+            email=(data['email'] if 'email' in data else None),
+            fan_page=(data['fan_page'] if 'fan_page' in data else None),
+            instagram=(data['instagram'] if 'instagram' in data else None),
+            line=(data['line'] if 'line' in data else None),
+            telephone=(data['telephone'] if 'telephone' in data else None),
+            website=(data['website'] if 'website' in data else None),
+            youtube=(data['youtube'] if 'youtube' in data else None),
+        )
+        g.db_session.add(row)
+        g.db_session.flush()
+        studio.social_id = row.id
+        g.db_session.add(studio)
+        g.db_session.commit()
+        res = row.as_dict()
+    except Exception as err:
+        app.logger.error(err)
+        g.db_session.rollback()
+
+        return {
+            'code': 'ERROR_STUDIO_6',
+            'description': str(err),
+            'http_status_code': HTTPStatus.BAD_REQUEST,
+            'status': 'ERROR',
+        }
+    else:
+        return {
+            'code': 'ROUTE_AUTH_3',
+            'data': res,
+            'http_status_code': HTTPStatus.CREATED,
             'status': 'SUCCESS',
         }
