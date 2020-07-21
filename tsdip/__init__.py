666a94a89564e469aa86cc18da0bf44ebe8b34f8
Edwin Jarvis <fenglan.crazydance@gmail.com>
2020-07-21 21:44:57 +0800

    Update new database schema
    Signed-off-by: Edwin Jarvis <fenglan.crazydance@gmail.com>

--- tsdip/__init__.py
+++ tsdip/__init__.py
@@ -7,6 +7,16 @@ def create_app(config=None):
     app.config.from_object(config)
     app.app_context().push()
 
+    db.init_app(app)
+
+    # from .routes.event import api_blueprint as event_blueprint
+    from .routes.manager import api_blueprint as manager_blueprint
+    # from .routes.studio import api_blueprint as studio_blueprint
+
+    # app.register_blueprint(event_blueprint)
+    app.register_blueprint(manager_blueprint)
+    # app.register_blueprint(studio_blueprint)
+
     @app.route('/hello')
     def hello_world():
         return jsonify({'text': 'Hello, World!'})
--- tsdip/models.py
+++ tsdip/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import CheckConstraint, func, text
 from sqlalchemy.dialects.postgresql import ENUM, TIMESTAMP, UUID
 from sqlalchemy.orm import validates
+from sqlalchemy.sql import expression
 
 from tsdip import db
 
@@ -28,11 +29,17 @@ class Base():
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+    def as_dict(self, filter_at=False):
+        temp = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        if filter_at:
+            del temp['created_at']
+            del temp['updated_at']
+            del temp['deleted_at']
+
+        return temp
 
 
-class Social(Base, db.Model):
+class Social(db.Model, Base):
     email = db.Column(db.String(255), unique=True)
     fan_page = db.Column(db.String(255), unique=True)
     instagram = db.Column(db.String(255), unique=True)
@@ -46,43 +53,97 @@ class Social(Base, db.Model):
         return value.lower()
 
 
-class RequestLog(Base, db.Model):
-    request = db.Column(
-        ENUM('event', 'invite', 'manager', 'studio', name='request_type'),
+class Event(db.Model, Base):
+    __table_args__ = (
+        CheckConstraint('amount > -1'),
+        CheckConstraint('price > -1'),
+    )
+
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    description = db.Column(db.Text)
+    amount = db.Column(db.Integer, nullable=False, server_default='0')
+    price = db.Column(db.Integer, nullable=False, server_default='0')
+    reg_link = db.Column(db.String(128), unique=True)
+    reg_start_at = db.Column(TIMESTAMP)
+    reg_end_at = db.Column(TIMESTAMP)
+    start_at = db.Column(TIMESTAMP)
+    end_at = db.Column(TIMESTAMP)
+
+    org_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey('organization.id', onupdate='CASCADE',
+                      ondelete='CASCADE')
+    )
+    ogr = db.relationship('Organization', uselist=False)
+    social_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey('social.id', onupdate='CASCADE', ondelete='CASCADE')
+    )
+    social = db.relationship('Social', uselist=False)
+
+
+class Organization(db.Model, Base):
+    name = db.Column(db.String(255), nullable=False, unique=True)
+    description = db.Column(db.Text)
+    address = db.Column(db.String(255))
+    org_type = db.Column(
+        ENUM('dance_group', 'studio', name='org_type'),
         nullable=False,
-        server_default='event'
+        server_default='studio'
     )
-    request_id = db.Column(UUID(as_uuid=True), nullable=False)
-    approve = db.Column(db.Boolean, nullable=False, server_default='False')
-    approve_at = db.Column(TIMESTAMP)
 
-    approve_by = db.Column(
+    social_id = db.Column(
         UUID(as_uuid=True),
-        db.ForeignKey('manager.id', onupdate='CASCADE', ondelete='CASCADE')
+        db.ForeignKey('social.id', onupdate='CASCADE', ondelete='CASCADE')
     )
-    approver = db.relationship('Manager', uselist=False)
+    social = db.relationship('Social', uselist=False)
+    events = db.relationship('Event', lazy=True)
 
 
-permission = db.Table(
-    'permission',
-    db.metadata,
-    db.Column(
-        'manager_id',
-        UUID(as_uuid=True),
-        db.ForeignKey('manager.id', onupdate='CASCADE', ondelete='CASCADE'),
-        primary_key=True
-    ),
-    db.Column(
-        'studio_id',
-        UUID(as_uuid=True),
-        db.ForeignKey('studio.id', onupdate='CASCADE', ondelete='CASCADE'),
-        primary_key=True
-    ),
-    db.Column(
-        'role',
-        ENUM('manager', 'owner', 'viewer', name='permission_role'),
+class Permission(db.Model, Base):
+    could_invite = db.Column(db.Boolean, nullable=False,
+                             server_default=expression.false())
+
+
+class Role(db.Model, Base):
+    __table_args__ = (
+        db.UniqueConstraint('org_id', 'permission_id',
+                            name='uq_role_org_id_permission_id'),
+    )
+
+    name = db.Column(
+        ENUM('owner', 'manager', 'viewer', name='role_name'),
         nullable=False,
         server_default='viewer'
+    )
+    org_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey('organization.id', onupdate='CASCADE',
+                      ondelete='CASCADE')
+    )
+    permission_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey('permission.id', onupdate='CASCADE', ondelete='CASCADE')
+    )
+
+    org = db.relationship('Organization', uselist=False)
+    permission = db.relationship('Permission', uselist=False)
+
+
+user_role = db.Table(
+    'user_role',
+    db.metadata,
+    db.Column(
+        'user_id',
+        UUID(as_uuid=True),
+        db.ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'),
+        primary_key=True
+    ),
+    db.Column(
+        'role_id',
+        UUID(as_uuid=True),
+        db.ForeignKey('role.id', onupdate='CASCADE', ondelete='CASCADE'),
+        primary_key=True
     ),
     db.Column(
         'created_at',
@@ -104,63 +165,82 @@ permission = db.Table(
 )
 
 
-class Manager(Base, db.Model):
+class User(db.Model, Base):
     username = db.Column(db.String(255), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     telephone = db.Column(db.String(20), unique=True)
 
-    studios = db.relationship(
-        'Studio',
-        secondary=permission,
-        lazy='dynamic'
-    )
+    roles = db.relationship('UserRole', secondary=user_role, lazy='dynamic')
 
     @validates('email')
     def convert_lower(self, key, value):
         return value.lower()
 
 
-class Studio(Base, db.Model):
-    name = db.Column(db.String(255), nullable=False, unique=True)
-    description = db.Column(db.Text)
-    address = db.Column(db.String(255))
-    studio_type = db.Column(
-        ENUM('dance_group', 'studio', name='studio_type'),
-        nullable=False,
-        server_default='studio'
-    )
-
-    social_id = db.Column(
+class RequestOrgLog(db.Model, Base):
+    req_type = db.Column(db.String(255), nullable=False)
+    org_id = db.Column(
         UUID(as_uuid=True),
-        db.ForeignKey('social.id', onupdate='CASCADE', ondelete='CASCADE')
+        db.ForeignKey('organization.id', onupdate='CASCADE',
+                      ondelete='CASCADE')
     )
-    social = db.relationship('Social', uselist=False)
-    events = db.relationship('Event', lazy=True)
-
-
-class Event(Base, db.Model):
-    __table_args__ = (
-        CheckConstraint('amount > -1'),
-        CheckConstraint('price > -1'),
-    )
-
-    name = db.Column(db.String(255), nullable=False, unique=True)
-    description = db.Column(db.Text)
-    amount = db.Column(db.Integer, nullable=False, server_default='0')
-    price = db.Column(db.Integer, nullable=False, server_default='0')
-    reg_link = db.Column(db.String(128), unique=True)
-    reg_start_at = db.Column(TIMESTAMP)
-    reg_end_at = db.Column(TIMESTAMP)
-    start_at = db.Column(TIMESTAMP)
-    end_at = db.Column(TIMESTAMP)
-
-    studio_id = db.Column(
+    applicant_id = db.Column(
         UUID(as_uuid=True),
-        db.ForeignKey('studio.id', onupdate='CASCADE', ondelete='CASCADE')
+        db.ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE')
     )
-    studio = db.relationship('Studio', uselist=False)
-    social_id = db.Column(
+    approver_id = db.Column(
         UUID(as_uuid=True),
-        db.ForeignKey('social.id', onupdate='CASCADE', ondelete='CASCADE')
+        db.ForeignKey('manager.id', onupdate='CASCADE', ondelete='CASCADE')
     )
-    social = db.relationship('Social', uselist=False)
+    approve_at = db.Column(TIMESTAMP)
+
+    applicant = db.relationship('User', uselist=False)
+    approver = db.relationship('Manager', uselist=False)
+    org = db.relationship('Organization', uselist=False)
+
+
+class RequestEventLog(db.Model, Base):
+    req_type = db.Column(db.String(255), nullable=False)
+    event_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey('event.id', onupdate='CASCADE', ondelete='CASCADE')
+    )
+    applicant_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE')
+    )
+    approver_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey('manager.id', onupdate='CASCADE', ondelete='CASCADE')
+    )
+    approve_at = db.Column(TIMESTAMP)
+
+    applicant = db.relationship('User', uselist=False)
+    approver = db.relationship('Manager', uselist=False)
+    event = db.relationship('Event', uselist=False)
+
+
+class RequestMemberLog(db.Model, Base):
+    req_type = db.Column(db.String(255), nullable=False)
+    inviter_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE')
+    )
+    invitee_id = db.Column(
+        UUID(as_uuid=True),
+        db.ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE')
+    )
+    invited_at = db.Column(TIMESTAMP)
+
+    inviter = db.relationship('User', uselist=False)
+    invitee = db.relationship('User', uselist=False)
+
+
+class Manager(db.Model, Base):
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    telephone = db.Column(db.String(20), unique=True)
+
+    @validates('email')
+    def convert_lower(self, key, value):
+        return value.lower()
